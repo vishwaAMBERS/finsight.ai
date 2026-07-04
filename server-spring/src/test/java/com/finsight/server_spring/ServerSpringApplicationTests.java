506597505181da293820b7bc14a9bc52b5356d9a
vishwaAMBERS <vishwamulge181@gmail.com>
2026-07-04 16:40:01 +0530

    Day 1 - project setup, Node.js init, Spring Boot generated

---
+++ server-spring/src/test/java/com/finsight/server_spring/ServerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsight.server_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
